3799da69e5f97fd8ec2a46076d19bc6466f8b6cb
Anthony17DEV <109322252+Anthony17DEV@users.noreply.github.com>
2022-08-12 08:56:23 -0300

    Create Palindromo.C

---
+++ Palindromo.C
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(){
+	
+	int  i, valor = 0;
+	char palavra[15], inversa[15];
+	
+	printf("\nDigite uma palavra:");
+	gets(palavra);
+	
+	for(i = 0; palavra[i]; i++){
+		palavra[i] = tolower(palavra[i]);
+	}
+	
+	strcpy(inversa, palavra);
+	strrev(inversa);
+	
+	valor = strcmp(palavra, inversa);
+	
+	if(valor == 0)
+		printf("\nA palavra %s eh palindroma\n", palavra);
+	else{
+		printf("\nA palavra %s nao eh palindroma\n", palavra);
+	}
+	
+	return 0;
+	
+}
